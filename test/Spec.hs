a9eab85f022e038549d3414afcc0a4db1c20c4ea
John P Martin <jpm61704@gmail.com>
2018-07-19 18:15:33 -0600

    implementation of testing

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,10 @@
+import Test.Hspec
+import HMM
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = hspec $ do
+  describe "HMM" $ do
+    it "prob1 evaluates trial1 properly" $ do
+      prob1 exampleHMM trial1 `shouldBe` 0.00003951627542528
+    it "prob1 evaluates trial2 properly" $ do
+      prob1 exampleHMM trial2 `shouldBe` 0.000035757147508736
